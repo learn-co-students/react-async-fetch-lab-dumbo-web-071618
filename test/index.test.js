dd51aefbea45074b528ee3822d3493f70ad588f8
github username <github email address>
2018-09-25 10:50:38 -0400

    Done.

--- test/index.test.js
+++ test/index.test.js
@@ -13,41 +13,41 @@ import App from '../src/App';
 
 
 describe('<App />', () => {
-  let appWrapper
-  let fetchSpy
+	let appWrapper
+	let fetchSpy
 
-  before(() => {
-    global.fetch = require('node-fetch')
-    fetchMock.get('*', {people: [{name:"Stimpy"}]})
-    fetchSpy = spy(global, "fetch")
+	before(() => {
+		global.fetch = require('node-fetch')
+		fetchMock.get('*', { people: [{ name: "Stimpy" }] })
+		fetchSpy = spy(global, "fetch")
 
-  })
-
-
-
-  //tests specific method within a class
-  it('mounts correctly', () => {
-
-    appWrapper = shallow(<App />)
-    expect(appWrapper).to.exist
-
-  });
-
-  it('calls componentDidMount and fetch', () => {
-
-    //set spy on a particular component's class method
-    spy(App.prototype, "componentDidMount")
-    // const fetch = fetchMock.sandbox()
-    //example shallow mount of a component
-
-    appWrapper = shallow(<App />);
-
-    expect(App.prototype.componentDidMount.calledOnce, "componentDidMount was not called").to.equal(true);
-
-    expect(fetchSpy.callCount > 0, "Fetch was not called").to.equal(true);
-
-    expect(fetchSpy.firstCall.lastArg).to.equal('http://api.open-notify.org/astros.json')
+	})
 
 
-  })
+
+	//tests specific method within a class
+	it('mounts correctly', () => {
+
+		appWrapper = shallow(<App />)
+		expect(appWrapper).to.exist
+
+	});
+
+	it('calls componentDidMount and fetch', () => {
+
+		//set spy on a particular component's class method
+		spy(App.prototype, "componentDidMount")
+		// const fetch = fetchMock.sandbox()
+		//example shallow mount of a component
+
+		appWrapper = shallow(<App />);
+
+		expect(App.prototype.componentDidMount.calledOnce, "componentDidMount was not called").to.equal(true);
+
+		expect(fetchSpy.callCount > 0, "Fetch was not called").to.equal(true);
+
+		expect(fetchSpy.firstCall.lastArg).to.equal('http://api.open-notify.org/astros.json')
+
+
+	})
 })
